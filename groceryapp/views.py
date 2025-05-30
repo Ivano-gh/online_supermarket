9ae75136346313602c7c215ae31b83166c8393b2
Ivano_gh <ivanbotchway31@gmail.com>
2025-05-30 08:07:39 +0000

    Initial commit: Added all project files

---
+++ groceryapp/views.py
@@ -0,0 +1,178 @@
+from django.shortcuts import render,redirect
+from .models import Carousel
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from .models import Category,Product
+from django.contrib.auth.models import User  # For User model  # If UserProfile is in models, add it here too
+from .models import UserProfile 
+from django.contrib.auth.decorators import login_required
+
+
+
+# Create your views here.
+
+def home(request):
+    return render(request,'home.html')
+
+
+@login_required
+def user_dashboard(request):
+    # This is your user dashboard
+    return render(request, 'user_dashboard.html')
+
+def index(request):
+    return render(request,'navigation.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def main(request):
+    data = Carousel.objects.all()
+    dic = {'data':data}
+    return render(request, 'index.html', dic)
+
+def adminLogin(request):
+    msg = None
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        try:
+            if user.is_staff:
+                login(request, user)
+                messages.success(request, "You have successfully logged in")
+                return redirect('admindashboard')
+            else:
+                messages.success(request, "Invalid Credentials")
+        except:
+            messages.success(request, "Invalid Credentials")
+    
+    return render(request, 'admin_login.html')
+
+def adminHome(request):
+    return render(request, 'admin_base.html')
+
+def admin_dashboard(request):
+    return render(request, 'admin_dashboard.html')
+
+
+def add_category(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        Category.objects.create(name=name)
+        messages.success(request, "Category added")
+        return redirect('view_category')
+    return render(request, 'add_category.html', locals())
+ 
+def view_category(request):
+    category = Category.objects.all()
+    return render(request, 'view_category.html', locals())
+
+def edit_category(request, pid):
+    category = Category.objects.get(id=pid)
+    if request.method == "POST":
+        name = request.POST['name']
+        category.name = name
+        category.save()
+        messages.success(request, "Category updated")
+        return redirect('view_category')
+    return render(request, 'edit_category.html', locals())
+
+def delete_category(request, pid):
+    category = Category.objects.get(id=pid)
+    category.delete()
+    messages.success(request, "Category deleted")
+    return redirect('view_category')
+
+
+def add_product(request):
+    category = Category.objects.all()
+    if request.method == "POST":
+        name = request.POST['name']
+        price = request.POST['price']
+        cat = request.POST['category']
+        discount = request.POST['discount']
+        desc = request.POST['desc']
+        image = request.FILES['image']
+        catobj = Category.objects.get(id=cat)
+        Product.objects.create(name=name, price=price, discount=discount, category=catobj, description=desc, image=image)
+        messages.success(request, "Product added")
+    return render(request, 'add_product.html', locals())
+
+def view_product(request):
+    product = Product.objects.all()
+    return render(request, 'view_product.html', locals())
+
+def edit_product(request, pid):
+    product = Product.objects.get(id=pid)
+    category = Category.objects.all()
+    if request.method == "POST":
+        name = request.POST['name']
+        price = request.POST['price']
+        cat = request.POST['category']
+        discount = request.POST['discount']
+        desc = request.POST['desc']
+        try:
+            image = request.FILES['image']
+            product.image = image
+            product.save()
+        except:
+            pass
+        catobj = Category.objects.get(id=cat)
+        Product.objects.filter(id=pid).update(name=name, price=price, discount=discount, category=catobj, description=desc)
+        messages.success(request, "Product Updated")
+        return redirect('view_product')
+    return render(request, 'edit_product.html', locals())
+
+def delete_product(request, pid):
+    product = Product.objects.get(id=pid)
+    product.delete()
+    messages.success(request, "Product Deleted")
+    return redirect('view_product')
+
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .models import UserProfile
+
+def registration(request):
+    if request.method == "POST":
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        address = request.POST.get('address')
+        mobile = request.POST.get('mobile')
+        image = request.FILES.get('image')  # Safely get the image
+        
+        # Create user and profile
+        user = User.objects.create_user(
+            username=email, 
+            first_name=fname, 
+            last_name=lname, 
+            email=email, 
+            password=password
+        )
+
+        # If image is provided, use it; otherwise, create the profile without it
+        UserProfile.objects.create(
+            user=user, 
+            mobile=mobile, 
+            address=address, 
+            image=image if image else None
+        )
+
+        messages.success(request, "Registration Successful")
+    return render(request, 'registration.html', locals())
+
+def userlogin(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request, user)
+            messages.success(request, "User login successfully")
+            return redirect('user_dashboard')  # Redirect to dashboard, not main
+        else:
+            messages.error(request, "Invalid Credentials")
+    return render(request, 'login.html')
---
+++ online_shopping_supermarket/urls.py
@@ -0,0 +1,49 @@
+"""
+URL configuration for online_shopping_supermarket project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from groceryapp.views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home, name="home"),
+    path('index/', index, name="index"),
+    path('about/', about, name="about"),
+    path('main/', main, name="main"),
+    path('admin-login/', adminLogin, name="admin_login"),
+    path('adminhome/', adminHome, name="adminhome"),
+    path('admindashboard/', admin_dashboard, name="admindashboard"),
+    path('add-category/', add_category, name="add_category"),
+    path('view-category/', view_category, name="view_category"),
+    path('edit-category/<int:pid>/', edit_category, name="edit_category"),
+    path('delete-category/<int:pid>/', delete_category, name="delete_category"),
+    path('add-product/', add_product, name='add_product'),
+    path('view-product/', view_product, name='view_product'),
+    path('edit-product/<int:pid>/', edit_product, name="edit_product"),
+    path('delete-product/<int:pid>/', delete_product, name="delete_product"),
+    path('registration/', registration, name="registration"),
+    path('userlogin/', userlogin, name="userlogin"),
+     path('dashboard/', user_dashboard, name='user_dashboard'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
