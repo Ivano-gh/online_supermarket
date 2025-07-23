2c266d6b93e889893ac740068c0983e85be39467
Ivano_gh <ivanbotchway31@gmail.com>
2025-07-23 12:07:25 +0000

    Making sure that my media files show

--- online_shopping_supermarket/urls.py
+++ online_shopping_supermarket/urls.py
@@ -1,24 +1,9 @@
-"""
-URL configuration for online_shopping_supermarket project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include, re_path
 from groceryapp.views import *
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -57,17 +42,22 @@ urlpatterns = [
     path('manage-feedback/', manage_feedback, name="manage_feedback"),
     path('delete-feedback/<int:pid>/', delete_feedback, name="delete_feedback"),
     path('payment/', payment, name="payment"), 
-    path('payment/verify/',verify_payment, name='verify_payment'),
+    path('payment/verify/', verify_payment, name='verify_payment'),
     path('manage-order/', manage_order, name="manage_order"),
     path('delete-order/<int:pid>/', delete_order, name="delete_order"), 
     path('admin-order-track/<int:pid>/', admin_order_track, name="admin_order_track"),
     path('manage-user/', manage_user, name="manage_user"),
     path('delete-user/<int:pid>/', delete_user, name="delete_user"),
-    path('admin-change-password/',admin_change_password, name="admin_change_password"),
+    path('admin-change-password/', admin_change_password, name="admin_change_password"),
     path('mark-feedback-read/<int:pid>/', mark_feedback_read, name='mark_feedback_read'),
     path('contact/', contact, name="contact"),
     path('products/', home_products, name='home_products'),
-    
 ]
 
+# Serve media files during development
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Serve media files in production (e.g., on Render)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
